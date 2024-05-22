1ff40a54a5d9059349d9dd03522d61a22087ba4e
imm4d3 <135858911+imm4d3@users.noreply.github.com>
2024-05-22 23:46:33 +0300

    init

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const authBtn = document.querySelector('.auth-btn');
+    authBtn.addEventListener('click', () => {
+        alert('Функция входа будет реализована');
+    });
+
+    const cartBtn = document.querySelector('.cart-btn');
+    cartBtn.addEventListener('click', () => {
+        alert('Функция корзины будет реализована');
+    });
+});
