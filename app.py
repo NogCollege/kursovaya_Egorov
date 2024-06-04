e2ed19fc3bd33ef7056cedf1a12f7151afd31405
imm4d3 <135858911+imm4d3@users.noreply.github.com>
2024-06-04 12:47:10 +0300

    много чиво сделано очинь много

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import sqlite3
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -12,43 +12,56 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-conn = sqlite3.connect('instance/database.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT UNIQUE NOT NULL,
-                    password TEXT NOT NULL,
-                    is_admin TEXT NOT NULL)''')
-conn.commit()
-conn.close()
+DATABASE = 'instance/database.db'
+
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            username TEXT UNIQUE NOT NULL,
+                            password TEXT NOT NULL,
+                            is_admin TEXT NOT NULL,
+                            is_courier TEXT NOT NULL)''')
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS orders (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            promocode TEXT NOT NULL,
+                            discount INTEGER NOT NULL)''')
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS sales (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT NOT NULL,
+                            desc TEXT NOT NULL)''')
+
+init_db()
 
 class User(UserMixin):
-    def __init__(self, id, username, password, is_admin):
+    def __init__(self, id, username, password, is_admin, is_courier):
         self.id = id
         self.username = username
         self.password = password
         self.is_admin = is_admin
+        self.is_courier = is_courier
 
     @staticmethod
     def get(user_id):
-        conn = sqlite3.connect('instance/database.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-        user = cursor.fetchone()
-        conn.close()
-        if user:
-            return User(*user)
+        with sqlite3.connect(DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+            user = cursor.fetchone()
+            if user:
+                return User(*user)
         return None
 
     @staticmethod
     def find_by_username(username):
-        conn = sqlite3.connect('instance/database.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-        user = cursor.fetchone()
-        conn.close()
-        if user:
-            return User(*user)
+        with sqlite3.connect(DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+            user = cursor.fetchone()
+            if user:
+                return User(*user)
         return None
 
 @login_manager.user_loader
@@ -59,21 +72,17 @@ def load_user(user_id):
 def index():
     return render_template('index.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        logging.debug('Login form submitted.')
         username = request.form['username']
         password = request.form['password']
         user = User.find_by_username(username)
         if user and check_password_hash(user.password, password):
-            logging.debug('User authenticated successfully.')
             login_user(user)
             return redirect(url_for('index'))
         else:
-            logging.debug('Authentication failed.')
-            flash('Неправильное имя пользователя или пароль.', 'error')
+            flash('Неверное имя пользователя или пароль.', 'error')
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -83,26 +92,140 @@ def register():
         password = request.form['password']
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         try:
-            conn = sqlite3.connect('instance/database.db')
-            cursor = conn.cursor()
-            cursor.execute('''INSERT INTO users (username, password, is_admin)
-                              VALUES (?, ?, ?)''', (username, hashed_password, "no"))
-            conn.commit()
-            conn.close()
-
-            flash('Регистрация прошла успешно. Теперь вы можете войти.', 'success')
+            with sqlite3.connect(DATABASE) as conn:
+                cursor = conn.cursor()
+                cursor.execute('''INSERT INTO users (username, password, is_admin, is_courier) VALUES (?, ?, ?, ?)''', (username, hashed_password, "no", "no"))
+                conn.commit()
+            flash('Регистрация завершена. Теперь вы можете авторизоваться.', 'success')
             return redirect(url_for('login'))
         except sqlite3.IntegrityError:
-            flash('Имя пользователя уже существует.', 'error')
-            return redirect(url_for('login'))
+            flash('Пользователь с таким именем уже существует', 'error')
+            return redirect(url_for('register'))
     return render_template('register.html')
-
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+def admin_required(f):
+    @login_required
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin == "yes":
+            flash('У вас нет прав для просмотра данной страницы.', 'error')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    decorated_function.__name__ = f.__name__
+    return decorated_function
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    flash('Для просмотра данной страницы вам нужно авторизоваться.', 'error')
+    return redirect(url_for('login'))
+
+@app.route('/admin', methods=['GET', 'POST'])
+@admin_required
+def admin_panel():
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT promocode, discount FROM orders')
+        promo_codes = cursor.fetchall()
+        cursor.execute('SELECT name, desc FROM sales')
+        sales = cursor.fetchall()
+    return render_template('panel.html', current_user=current_user, sales=sales, promo_codes=promo_codes, message=None)
+
+@app.route('/change_admin_status', methods=['POST'])
+@admin_required
+def change_admin_status():
+    username = request.form['username']
+    action = request.form['action']
+    new_status = 'yes' if action == 'grant' else 'no'
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('UPDATE users SET is_admin = ? WHERE username = ?', (new_status, username))
+        if cursor.rowcount == 0:
+            flash(f"Пользователь {username} не найден.", 'error')
+        else:
+            conn.commit()
+            action_text = 'granted' if action == 'grant' else 'revoked'
+            flash(f"Админ-панель {action_text} для пользователя {username}.", 'success')
+    return redirect(url_for('admin_panel'))
+
+def courier_required(f):
+    @login_required
+    def decorated_function(*args, **kwargs):
+        if not (current_user.is_courier == "yes" or current_user.is_admin == "yes"):
+            flash('У вас нет прав для просмотра данной страницы.', 'error')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    decorated_function.__name__ = f.__name__
+    return decorated_function
+
+@app.route('/courier', methods=['GET', 'POST'])
+@courier_required
+def courier_panel():
+    return render_template('couriers.html', current_user=current_user, message=None)
+
+@app.route('/change_courier_status', methods=['POST'])
+@admin_required
+def change_courier_status():
+    courier_username = request.form['courier_username']
+    courier_password = request.form['courier_password']
+    hashed_password = generate_password_hash(courier_password, method='pbkdf2:sha256')
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO users (username, password, is_admin, is_courier) VALUES (?, ?, ?, ?)''', (courier_username, hashed_password, "no", "yes"))
+        conn.commit()
+        flash('Аккаунт для курьера успешно создан.', 'success')
+    return redirect(url_for('admin_panel'))
+
+@app.route('/cart', methods=['GET'])
+@login_required
+def cart():
+    return render_template('cart.html')
+
+@app.route('/create_delete_promocode', methods=['POST'])
+@admin_required
+def create_delete_promocode():
+    promocode = request.form['promocode']
+    discount = int(request.form['discount'])
+    action = request.form['action2']
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        if action == 'create':
+            cursor.execute('''INSERT INTO orders (promocode, discount) VALUES (?, ?)''', (promocode, discount))
+            conn.commit()
+            flash(f"Промокод {promocode} успешно создан.", 'success')
+        else:
+            cursor.execute('''DELETE FROM orders WHERE promocode = ? AND discount = ?''', (promocode, discount))
+            conn.commit()
+            if cursor.rowcount == 0:
+                flash(f"Промокод {promocode} не найден.", 'error')
+            else:
+                flash(f"Промокод {promocode} успешно удален.", 'success')
+    return redirect(url_for('admin_panel'))
+
+@app.route('/create_delete_sale', methods=['POST'])
+@admin_required
+def create_delete_sale():
+    promotion = request.form['promotion_name']
+    description = request.form['promotion_description']
+    action = request.form['action3']
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor() 
+        if action == 'create':
+            cursor.execute('''INSERT INTO sales (name, desc) VALUES (?, ?)''', (promotion, description))
+            conn.commit()
+            flash("Акция успешно создана.", 'success')
+        else:
+            cursor.execute('''DELETE FROM sales WHERE name = ? AND desc = ?''', (promotion, description))
+            conn.commit()
+            if cursor.rowcount == 0:
+                flash("Акция не найдена.", 'error')
+            else:
+                flash("Акция успешно удалена.", 'success')
+    return redirect(url_for('admin_panel'))
 
 if __name__ == '__main__':
     app.run(debug=True)
