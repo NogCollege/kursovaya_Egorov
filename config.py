c000f16d848a6c5766447c7ba699b50c21d576b8
imm4d3 <135858911+imm4d3@users.noreply.github.com>
2024-06-13 19:20:38 +0300

    initital release
    ...

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SECRET_KEY = os.urandom(24)
+    DATABASE = 'instance/database.db'
+    UPLOAD_FOLDER = os.path.join('static', 'uploads')
+    ADMIN_EMAIL = 'yasip@internet.ru'
+    STATUS_LIST = [
+        "Готовится",
+        "Готов, курьер спешит за ним",
+        "Курьер забрал заказ и направляется к вам",
+        "Заказ доставлен"
+    ]
